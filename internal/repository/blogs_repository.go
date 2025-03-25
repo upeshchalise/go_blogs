f36eaacf4d434a13f56c83f39aabec6b65e6d8e7
upeshchalise <upeshchalise@gmail.com>
2025-03-25 22:14:55 +0545

    blogs in progress

--- internal/repository/blogs_repository.go
+++ internal/repository/blogs_repository.go
@@ -3,15 +3,16 @@ package repository
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/upeshchalise/go_blogs/internal/database"
 	"github.com/upeshchalise/go_blogs/internal/models"
 )
 
 type BlogsRepository interface {
 	CreateBlog(blog *models.Blog) error
-	// GetBlogById(id uuid.UUID) (*models.Blog, error)
-	// GetAllBlogs() ([]models.Blog, error)
-	// GetBlogsByUserId(userId uuid.UUID) ([]models.Blog, error)
+	GetBlogById(id uuid.UUID) (*models.Blog, error)
+	GetAllBlogs() ([]models.Blog, error)
+	GetBlogsByUserId(userId uuid.UUID) ([]models.Blog, error)
 }
 
 type blogsRepository struct{}
@@ -27,4 +28,32 @@ func (r *blogsRepository) CreateBlog(blog *models.Blog) error {
 	}
 
 	return database.DB.Create(blog).Error
+}
+
+func (r *blogsRepository) GetBlogById(id uuid.UUID) (*models.Blog, error) {
+	var blog models.Blog
+	if err := database.DB.First(&blog, id).Error; err != nil {
+
+		return nil, err
+	}
+
+	return &blog, nil
+}
+
+func (r *blogsRepository) GetAllBlogs() ([]models.Blog, error) {
+	var blogs []models.Blog
+	if err := database.DB.Find(&blogs).Error; err != nil {
+		return nil, err
+	}
+
+	return blogs, nil
+}
+
+func (r *blogsRepository) GetBlogsByUserId(userId uuid.UUID) ([]models.Blog, error) {
+	var blogs []models.Blog
+	if err := database.DB.Where("user_id = ?", userId).Find(&blogs).Error; err != nil {
+		return nil, err
+	}
+
+	return blogs, nil
 }
